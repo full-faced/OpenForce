6be4fa556c7b0cccf048a00e69f13b164090556e
Sean Nelson <smnelsonn@gmail.com>
2019-06-05 13:17:37 -0700

    user model tested

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -16,4 +16,14 @@ describe('user model testing', () => {
       _id: expect.any(mongoose.Types.ObjectId)
     });
   });
+
+  it('will not create a user without a username', () =>{
+    const badUser = new User({
+      password: 'testpassword',
+    });
+
+    const errors = badUser.validateSync().errors;
+    expect(errors.username.message).toEqual('Path `username` is required.');
+    expect(errors.email.message).toEqual('Path `email` is required.');
+  });
 });
