1376005c6ccb8637ac66098e66d53c54f50ab201
Sean Nelson <smnelsonn@gmail.com>
2019-06-05 11:41:57 -0700

    untokenized passing

--- test/utils/token.test.js
+++ test/utils/token.test.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
 const { tokenize, untokenize } = require('../../lib/utils/token');
 
 describe('token functionality', () => {
@@ -7,4 +6,11 @@ describe('token functionality', () => {
     const token = tokenize({ username: 'sean', email: 'email@email.com' });
     expect(token).toEqual(expect.any(String));
   });
+
+  it('pulls out a token', () => {
+    const token = tokenize({ username: '1', email: '2' });
+    const untokened = untokenize(token);
+    expect(untokened).toEqual({ username: '1', email: '2' });
+  });
 });
+
