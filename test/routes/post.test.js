c650a62ce933a3c0526d2e16946a9b4bb5f029e8
Cara Ponzini <ponzini@gmail.com>
2019-06-05 17:40:11 -0700

    resolved destructuring issue

--- test/routes/post.test.js
+++ test/routes/post.test.js
@@ -1,52 +1,52 @@
-// require('dotenv').config();
-// const request = require('supertest');
-// const mongoose = require('mongoose');
-// const app = require('../../lib/app');
+require('dotenv').config();
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../../lib/app');
 
-// describe('project routes tests', () => {
-//   const projectInfo = {
-//     title: 'title',
-//     summary: 'summary',
-//     projectUrl: 'projectUrl',
-//     imageUrl: 'image',
-//     posts: []
-//   };
+describe('project routes tests', () => {
+  const projectInfo = {
+    title: 'title',
+    summary: 'summary',
+    projectUrl: 'projectUrl',
+    imageUrl: 'image',
+    posts: []
+  };
 
-//   beforeAll(() => {
-//     return mongoose.connect('mongodb://localhost:27017/openForce', {
-//       useCreateIndex: true,
-//       useFindAndModify: false,
-//       useNewUrlParser: true
-//     });
-//   });
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27017/openForce', {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true
+    });
+  });
 
-//   beforeEach(() => {
-//     return mongoose.connection.dropDatabase();
-//   });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
 
-//   afterAll(() => {
-//     return mongoose.connection.close();
-//   });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
 
-//   it.only('creates a post with user/project', () => {
-//     return request(app)
-//       .post('/auth/signup')
-//       .send({ username: 'test', password: 'testp', email: 'teste' })
-//       .then(createdUser => {
-//         console.log(createdUser);
-//         return request(app)
-//           .post('/projects/')
-//           .send({ ...projectInfo, user: createdUser.body.user._id })
-//           .set('Authorization', `Bearer ${createdUser.body.token}`)
-//           .then(createdProject => {
-//             return request(app)
-//               .post('/posts/')
-//               .send({ user: createdUser.body.user._id, project: createdProject.body.project._id, text: 'holy shit' })
-//               .set('Authorization', `Bearer ${createdUser.body.token}`)
-//               .then(createdPost => {
-//                 console.log(createdPost);
-//               });
-//           });
-//       });
-//   });
-// });
+  it.only('creates a post with user/project', () => {
+    return request(app)
+      .post('/auth/signup')
+      .send({ username: 'test', password: 'testp', email: 'teste' })
+      .then(createdUser => {
+        return request(app)
+          .post('/projects/')
+          .send({ ...projectInfo, user: createdUser.body.user._id })
+          .set('Authorization', `Bearer ${createdUser.body.token}`)
+          .then(createdProject => {
+            return request(app)
+              .post('/posts/')
+              .send({ user: createdUser.body.user._id, project: createdProject.body.project._id, text: 'holy shit' })
+              .set('Authorization', `Bearer ${createdUser.body.token}`)
+              .then(createdPost => {
+                expect(createdPost.body.user).toEqual(createdUser.body.user._id);
+                expect(createdPost.body.project).toEqual(createdProject.body.project._id);
+              });
+          });
+      });
+  });
+});
